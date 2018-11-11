1f9e6400244519a1e554385ffe980a22cfaa6333
16222010168 <cyc870528@hotmail.com>
2018-11-11 10:00:11 +0800

    Update patrol.sh

--- patrol.sh
+++ patrol.sh
@@ -1,4 +1,3 @@
-# more patrol.sh
 #!/usr/bin/ksh 
 #
 #AUTHOR: CaoYuchi
@@ -14,15 +13,15 @@ check_errpt()
 MD=`date +%m%d`
 YEAR=`date +%y`
 echo "\n"
-echo "-----------Error Information-------------"
+echo "------------------------------------*Error Information*---------------------------------"
 ERRPT=$(errpt|grep "$MD....$YEAR")
 if [ -n "$ERRPT" ];then
   echo "System call out errorlog, pls pay attention!\n\n"
   echo "$ERRPT"
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "There's no error log today.\n"
-  echo "-------------------------------- \n\n\n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
@@ -30,15 +29,15 @@ fi
 check_fs()
 {
 #check the usage of the filesystem, each filesystem should not larger than 80%
-echo "---------------File system Information-----------------"
+echo "---------------------------------*File system Information*------------------------------"
 FILE=`df -g|grep -Ev "Filesystem"|sed 's/%//'|awk '{ if ($4 > 80) print $0 }'`
 if [ -z "$FILE" ];then
   echo "There's no FS larger than 80%."
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "Filesystem larger than 80%, pls pay attention!"
   echo "$FILE"
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
@@ -46,15 +45,15 @@ fi
 check_disk()
 {
 #check the availability of the disk.
-echo "----------------Disk Information----------------"
+echo "------------------------------------*Disk Information*----------------------------------"
 DISK=`lsdev -Cc disk|awk '{if ($2 !="Available") print $0}'`
 if [ -z "$DISK" ];then
   echo "The disk system is normal"
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "Disk system abnormal, pls pay attention!"
   echo "$DISK"
-  echo "-------------------------------- \n\n\n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
@@ -62,15 +61,15 @@ fi
 check_adapter()
 {
 #check the adapter status.
-echo "------------------Adapter Information--------------------"
+echo "------------------------------------*Adapter Information*-------------------------------"
 ADAPTER=`lsdev -Cc adapter|awk '{if ($2 !="Available") print $0}'`
 if [ -z "$ADAPTER" ];then
   echo "The adapters in this system are normal"
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "There are abnormal adapter, pls pay attention!"
   echo "$ADAPTER"
-  echo "-------------------------------- \n\n\n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
@@ -78,15 +77,15 @@ fi
 check_processor()
 {
 #check the processor status.
-echo "-------------------Processor Information-------------------"
+echo "-------------------------------*Processor Information*----------------------------------"
 PROCESSOR=`lsdev -Cc processor|awk '{if ($2 !="Available") print $0}'`
 if [ -z "$PROCESSOR" ];then
   echo "The processor is normal."
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "The processor is abnormal"
   echo "$PROCESSOR"
-  echo "-------------------------------- \n\n\n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
@@ -94,45 +93,45 @@ fi
 check_vgmirror()
 {
 #check the mirror of the lv
-echo "-------------------VG Mirror Information-------------  "
+echo "-----------------------------------*VG Mirror Information*------------------------------"
 MIRRORVG=`lsvg -l rootvg|grep -Ev "rootvg:"|grep -Ev "LV NAME" |awk '{ if ( $5 == 1 ) print $0}'`
 if [ -z "$MIRRORVG" ];then
   echo "There's no unmirror lv in rootvg."
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "There're unmirror lv in the rootvg, pls pay attention!"
   echo "$MIRRORVG"
-  echo "-------------------------------- \n\n\n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
 
 check_ps()
 {
-echo "--------------------------Paging Space Information-------------------------"
+echo "-----------------------------------*Paging Space Information*---------------------------"
 PS=`lsps -s|grep -Ev Total|sed 's/%//'|awk '{if ($2 > 70) print $0}'`
 if [ -z "$PS" ];then
   echo "There's no PS larger than 70%."
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
   echo "Paging space larger than 70%, pls pay attention!"
   echo "$PS"
-  echo "-------------------------------- \n"
+  echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 
 }
 
 check_rootvg_pv()
 {
-echo "--------------------------roovg pv Information-------------------------"
+echo "-----------------------------------*roovg pv Information*-------------------------------"
 rootvg_pv=`lsvg -p rootvg|sed '1,2d'|awk '{if ($2 != "active") print $0}'`
 if [ -z "$rootvg_pv" ];then
-    echo "no missing pv"
-        echo "-------------------------------- \n"
+    echo "No missing pv"
+        echo "---------------------------------------------------------------------------------------- \n\n\n"
 else
     echo "pv missing, pay attention"
         echo "$rootvg_pv"
-        echo "-------------------------------- \n"
+        echo "---------------------------------------------------------------------------------------- \n\n\n"
 fi
 }
 
@@ -147,3 +146,4 @@ check_processor|tee -a /tmp/Patrolog/`date +%m%d%H%M%y`.log
 check_vgmirror|tee -a /tmp/Patrolog/`date +%m%d%H%M%y`.log
 check_ps|tee -a /tmp/Patrolog/`date +%m%d%H%M%y`.log
 check_rootvg_pv|tee -a /tmp/Patrolog/`date +%m%d%H%M%y`.log
+
